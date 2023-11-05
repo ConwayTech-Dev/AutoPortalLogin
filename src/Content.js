5055a99fc8677d7c2d0190d3f8e0644fe3ad370a
Recontaminated <69064548+Recontaminated@users.noreply.github.com>
2023-11-04 17:59:31 -0700

    Update MyPolyAutoLogin version and add support for new login page

--- src/Content.js
+++ src/Content.js
@@ -213,7 +213,12 @@ console.log("logging in")
     document.body.appendChild(loadingDiv);
     window.location.href =
       "https://signin.blackbaud.com/signin/?sessionClear=true&redirectUrl=https:%2F%2Fpolytechnic.myschoolapp.com%2Fapp%3FsvcId%3Dedu%26envId%3Dp-QNcH02hZvE-V-xfBeGIQ4Q%26bb_id%3D1%23login";
-  } else if (window.location.href.includes("app.blackbaud.com/signin")) {
+  } 
+  
+  else if (window.location.href.includes("sso.myschoolapp.com")) {
+    document.body.append(loadingDiv);
+  }
+  else if (window.location.href.includes("app.blackbaud.com/signin")) {
     document.body.append(loadingDiv);
     await waitForElm(".spa-auth-button-full");
     document.getElementsByClassName("spa-auth-button-full")[0].click();
