4638a025091e66096a10609c913f2fbb2a5a7df0
Recontaminated <69064548+Recontaminated@users.noreply.github.com>
2024-02-12 11:59:42 -0800

    1.7 update

--- src/Content.js
+++ src/Content.js
@@ -18,91 +18,7 @@ function waitForElm(selector) {
     });
   });
 }
-loading_messages =   ["Rearranging the cosmos for your homework…",
-    "Loading your assignments faster than you can write 'procrastination'…",
-    "Brewing coffee for the late-night study sessions…",
-    "Polishing all the apples for the teachers…",
-    "Calculating the minimum amount of effort required for an A…",
-    "Taking a quick power nap before your next class…",
-    "Deciphering teacher's handwriting…",
-    "Getting our ducks in a row. And yes, we mean literal ducks…",
-    "Cranking up the motivation speakers to 11…",
-    "Loading motivational quotes… Error: too inspired to continue…",
-    "Counting all the pencils in the world for you to borrow…",
-    "Compressing all nighters into power naps…",
-    "Finding more creative excuses for not doing homework…",
-    "Downloading a sense of humor for the upcoming math class…",
-    "Inventing a time machine for last-minute assignment submissions…",
-    "Persuading your text books to open themselves…",
-    "Chasing down the dog that ate your homework…",
-    "Fluffing up the clouds in daydreams…",
-    "Sharpening digital pencils… because you never know…",
-    "Rewriting history, because your essay didn’t…",
-    "Juggling deadlines and procrastination like a circus act…",
-    "Engaging warp speed for this loading page…",
-    "Microwaving leftovers because study breaks are sacred…",
-    "Hiring a detective to find where the last hour went…",
-    "Trying to convince your assignments to complete themselves…",
-    "Upgrading your brain's software… Please wait…",
-    "Loading the smell of old books and new possibilities…",
-    "Uncovering the secrets of the Bermuda Triangle for your geography homework…",
-    "Training carrier pigeons to submit your assignments…",
-    "Bribing the Wi-Fi hamsters with extra seeds…",
-    "Digging through the library of Alexandria for your research paper…",
-    "Hunting down the elusive 'page 404' students have cited in their bibliography…",
-    "Consulting the Oracle for the answers to your next test…",
-    "Knocking on wood for those superstitious about exams…",
-    "Rescuing your focus from the clutches of social media…",
-    "Firing the cannon of success for your upcoming presentation…",
-    "Googling 'How to become a wizard' to deal with this term’s workload…",
-    "Stretching time so you can meet your deadline…",
-    "Assembling a team of scholarly ninjas for peer reviews…",
-    "Rolling out the red carpet for your GPA…",
-    "Training your brain to enjoy studying… by bribing it with chocolate…",
-    "Running a marathon on Encyclopedia pages… metaphorically…",
-    "Filling the idea tank to the brim…",
-    "Smuggling forbidden knowledge from the secret school archives…",
-    "Setting up a playdate with Plato and Socrates…",
-    "Organizing a pep rally for your morale…",
-    "Doing a rain dance for extra inspiration… because why not…",
-    "Whispering sweet nothings to the database for faster responses…",
-    "Using telepathy to contact the server… Results uncertain…",
-    "Enrolling pigeons in flight school to deliver your assignments…",
-    "Packing your backpack with ambition and snacks… 80% snacks…",
-    "Hiring squirrels to gather nuts of wisdom for the winter semester…",
-    "Loading… because even your computer needs a moment to think about calculus…",
-    "Asking a genie for more wishes... and more time for exam prep…",
-    "Crafting a cloak of invisibility for those days you want to avoid everyone…",
-    "Uploading the spirit of Einstein… Please stand by…",
-    "Consulting with wizards to make your notebook self-writing…",
-    "Trying to find the square root of pi for dessert…",
-    "Rounding up the homework gremlins. Don’t feed them after midnight…",
-    "Buffering… Just like your brain before the first coffee of the day…",
-    "Programming the robots to attend 8 AM classes for you…",
-    "Asking the magic 8-ball if you'll pass this semester… Outlook good…",
-    "Dusting off the dictionaries for that perfect essay word…",
-    "Tuning the instruments for your brain orchestra…",
-    "Pumping the jams for a homework dance break…",
-    "Inflating the balloons for your graduation party in advance…",
-    "Taming the wild essays into submission…",
-    "Negotiating with the alarm clock for five more minutes…",
-    "Gathering the dragon balls to wish for limitless knowledge…",
-    "Searching for the Holy Grail of uncopied homework ideas…",
-    "Unleashing the Kraken on your study distractions…",
-    "Sending carrier pigeons to remind you of due dates…",
-    "Drawing straws to see who has to do the group project work…",
-    "Paving the road to graduation with good intentions and late-night snacks…",
-    "Deploying search and rescue for your lost motivation…",
-    "Sending smoke signals to the server for faster load times…",
-    "Consulting tea leaves for the outcome of your next presentation…",
-    "Opening a portal for quick escapes from boring lectures…",
-]
-async function check() {
-  //   const userField = await waitForElm("#Username");
-  //   userField.value = "jhsieh25@students.polytechnic.org";
-  //   await waitForElm("#nextBtn");
-  // document.getElementById("nextBtn").click()
-}
+
 let loadingDiv = document.createElement("div");
 loadingDiv.innerHTML = `
 <section>
@@ -280,43 +196,45 @@ loadingDiv.style.display = "flex";
 loadingDiv.style.justifyContent = "center";
 loadingDiv.style.alignItems = "center";
 loadingDiv.style.zIndex = "1000000"; // high enough to overlay the entire content
+
+// document.body.appendChild(loadingDiv);
 document.addEventListener("DOMContentLoaded", async function (event) {
-console.log("logging in")
 
   window.onhashchange = function () {
-    console.log("window changed")
-    console.log(window.location.href)
-    if (window.location.href.includes("app#login") || window.location.href.includes("student#login")) {
-      if (document.querySelector("#site-header") == null) {
-        loadingDiv.remove();
-        console.log("logged in");
-        return
-      }
-      document.body.appendChild(loadingDiv);
-      window.location.href =
-        "https://signin.blackbaud.com/signin/?sessionClear=true&redirectUrl=https:%2F%2Fpolytechnic.myschoolapp.com%2Fapp%3FsvcId%3Dedu%26envId%3Dp-QNcH02hZvE-V-xfBeGIQ4Q%26bb_id%3D1%23login";
-    }
+    console.log("window changed");
+    console.log(window.location.href);
 
-      // cueck current url, if it contains "/app/student#studentmyday/progress" we logged in
-  if (
-    window.location.href.includes("https://polytechnic.myschoolapp.com/app/student#student")
-  ) {
+    
+
+  if (document.querySelector("#site-header") != null) {
     loadingDiv.remove();
-    console.log("logged in");
+    console.log("logged in bc we see site headre");
+    return;
   }
+
+    // cueck current url, if it contains "/app/student#studentmyday/progress" we logged in
+    if (
+      window.location.href.includes(
+        "https://polytechnic.myschoolapp.com/app/student#student"
+      )
+    ) {
+      loadingDiv.remove();
+      console.log("logged in");
+    }
   };
-  if (window.location.href.includes("app#login") || window.location.href.includes("student#login")) {
+  if (
+    window.location.href.includes("app#login") || window.location.href === "https://polytechnic.myschoolapp.com/app#login"
+  ) {
+
+    
     document.body.appendChild(loadingDiv);
     addQuoteToDiv();
     window.location.href =
       "https://signin.blackbaud.com/signin/?sessionClear=true&redirectUrl=https:%2F%2Fpolytechnic.myschoolapp.com%2Fapp%3FsvcId%3Dedu%26envId%3Dp-QNcH02hZvE-V-xfBeGIQ4Q%26bb_id%3D1%23login";
-  } 
-  
-  else if (window.location.href.includes("sso.myschoolapp.com")) {
+  } else if (window.location.href.includes("sso.myschoolapp.com")) {
     document.body.append(loadingDiv);
     addQuoteToDiv();
-  }
-  else if (window.location.href.includes("app.blackbaud.com/signin")) {
+  } else if (window.location.href.includes("app.blackbaud.com/signin")) {
     document.body.append(loadingDiv);
     addQuoteToDiv();
     await waitForElm(".spa-auth-button-full");
@@ -331,9 +249,8 @@ console.log("logging in")
     addQuoteToDiv();
     console.log("waiting for element to load");
     setTimeout(() => {
-
       // find all elements with [authuser] field and loop over them checking text content
-      let authUsers = document.querySelectorAll('[data-authuser]');
+      let authUsers = document.querySelectorAll("[data-authuser]");
       console.log(authUsers);
       for (let i = 0; i < authUsers.length; i++) {
         if (authUsers[i].innerText.includes("students.polytechnic.org")) {
@@ -341,12 +258,9 @@ console.log("logging in")
           break;
         }
       }
-
-
     }, 450);
-  
-  setInterval(() => {
 
+    setInterval(() => {
       let authUsers = document.querySelectorAll("[data-authuser]");
       console.log(authUsers);
       for (let i = 0; i < authUsers.length; i++) {
@@ -357,16 +271,93 @@ console.log("logging in")
       }
     }, 5000);
   }
-
-
-  
 });
 function addQuoteToDiv() {
+  loading_messages = [
+    "Rearranging the cosmos for your homework…",
+    "Loading your assignments faster than you can write 'procrastination'…",
+    "Brewing coffee for the late-night study sessions…",
+    "Polishing all the apples for the teachers…",
+    "Calculating the minimum amount of effort required for an A…",
+    "Taking a quick power nap before your next class…",
+    "Deciphering teacher's handwriting…",
+    "Getting our ducks in a row. And yes, we mean literal ducks…",
+    "Cranking up the motivation speakers to 11…",
+    "Loading motivational quotes… Error: too inspired to continue…",
+    "Counting all the pencils in the world for you to borrow…",
+    "Compressing all nighters into power naps…",
+    "Finding more creative excuses for not doing homework…",
+    "Downloading a sense of humor for the upcoming math class…",
+    "Inventing a time machine for last-minute assignment submissions…",
+    "Persuading your text books to open themselves…",
+    "Chasing down the dog that ate your homework…",
+    "Fluffing up the clouds in daydreams…",
+    "Sharpening digital pencils… because you never know…",
+    "Rewriting history, because your essay didn’t…",
+    "Juggling deadlines and procrastination like a circus act…",
+    "Engaging warp speed for this loading page…",
+    "Microwaving leftovers because study breaks are sacred…",
+    "Hiring a detective to find where the last hour went…",
+    "Trying to convince your assignments to complete themselves…",
+    "Upgrading your brain's software… Please wait…",
+    "Loading the smell of old books and new possibilities…",
+    "Uncovering the secrets of the Bermuda Triangle for your geography homework…",
+    "Training carrier pigeons to submit your assignments…",
+    "Bribing the Wi-Fi hamsters with extra seeds…",
+    "Digging through the library of Alexandria for your research paper…",
+    "Hunting down the elusive 'page 404' students have cited in their bibliography…",
+    "Consulting the Oracle for the answers to your next test…",
+    "Knocking on wood for those superstitious about exams…",
+    "Rescuing your focus from the clutches of social media…",
+    "Firing the cannon of success for your upcoming presentation…",
+    "Googling 'How to become a wizard' to deal with this term’s workload…",
+    "Stretching time so you can meet your deadline…",
+    "Assembling a team of scholarly ninjas for peer reviews…",
+    "Rolling out the red carpet for your GPA…",
+    "Training your brain to enjoy studying… by bribing it with chocolate…",
+    "Running a marathon on Encyclopedia pages… metaphorically…",
+    "Filling the idea tank to the brim…",
+    "Smuggling forbidden knowledge from the secret school archives…",
+    "Setting up a playdate with Plato and Socrates…",
+    "Organizing a pep rally for your morale…",
+    "Doing a rain dance for extra inspiration… because why not…",
+    "Whispering sweet nothings to the database for faster responses…",
+    "Using telepathy to contact the server… Results uncertain…",
+    "Enrolling pigeons in flight school to deliver your assignments…",
+    "Packing your backpack with ambition and snacks… 80% snacks…",
+    "Hiring squirrels to gather nuts of wisdom for the winter semester…",
+    "Loading… because even your computer needs a moment to think about calculus…",
+    "Asking a genie for more wishes... and more time for exam prep…",
+    "Crafting a cloak of invisibility for those days you want to avoid everyone…",
+    "Uploading the spirit of Einstein… Please stand by…",
+    "Consulting with wizards to make your notebook self-writing…",
+    "Trying to find the square root of pi for dessert…",
+    "Rounding up the homework gremlins. Don’t feed them after midnight…",
+    "Buffering… Just like your brain before the first coffee of the day…",
+    "Programming the robots to attend 8 AM classes for you…",
+    "Asking the magic 8-ball if you'll pass this semester… Outlook good…",
+    "Dusting off the dictionaries for that perfect essay word…",
+    "Tuning the instruments for your brain orchestra…",
+    "Pumping the jams for a homework dance break…",
+    "Inflating the balloons for your graduation party in advance…",
+    "Taming the wild essays into submission…",
+    "Negotiating with the alarm clock for five more minutes…",
+    "Gathering the dragon balls to wish for limitless knowledge…",
+    "Searching for the Holy Grail of uncopied homework ideas…",
+    "Unleashing the Kraken on your study distractions…",
+    "Sending carrier pigeons to remind you of due dates…",
+    "Drawing straws to see who has to do the group project work…",
+    "Paving the road to graduation with good intentions and late-night snacks…",
+    "Deploying search and rescue for your lost motivation…",
+    "Sending smoke signals to the server for faster load times…",
+    "Consulting tea leaves for the outcome of your next presentation…",
+    "Opening a portal for quick escapes from boring lectures…",
+  ];
   const now = new Date();
   const currentHour = now.getHours();
   const dayOfMonth = now.getDate();
 
   // Combine the day of the month with the current hour to get a unique index for each hour of each day
   let combinedIndex = (dayOfMonth * 24 + currentHour) % loading_messages.length;
-  document.querySelector('.quote').innerHTML = loading_messages[combinedIndex] ;
+  document.querySelector(".quote").innerHTML = loading_messages[combinedIndex];
 }
